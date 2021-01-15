911b84fccc922a95414bcde72395ab56f75f6bd7
Cristina <cristinna.iulia@gmail.com>
2021-01-15 12:50:58 +0200

    #12 added headers to logs details

--- include/Core/Request/Check_Email_Log_List_Action.php
+++ include/Core/Request/Check_Email_Log_List_Action.php
@@ -57,6 +57,10 @@ class Check_Email_Log_List_Action implements Loadie {
 					<td style="padding: 5px;"><?php _e( 'Subject', 'check-email' ); ?>:</td>
 					<td style="padding: 5px;"><?php echo esc_html( $log_item['subject'] ); ?></td>
 				</tr>
+                                <tr style="background: #eee;">
+					<td style="padding: 5px;"><?php _e( 'Headers', 'check-email' ); ?>:</td>
+					<td style="padding: 5px;"><?php echo esc_html( $log_item['headers'] ); ?></td>
+				</tr>
 
 				<?php
                                     do_action( 'check_email_view_log_after_headers', $log_item );
