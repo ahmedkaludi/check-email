a9361e5b06f8a7a2547d148874b683525e96d6c3
kumawatvikas <kumawatveykey@gmail.com>
2024-06-08 13:54:12 +0530

    Print the email log, or save it as a PDF #70
    Print the email log, or save it as a PDF #70

--- assets/js/admin/view-logs.js
+++ assets/js/admin/view-logs.js
@@ -43,3 +43,54 @@
 	} );
 
 })( jQuery );
+
+function showPrint() {
+	document.getElementById('check_mail_print_button').style.display = '';
+}
+function hidePrint() {
+	document.getElementById('check_mail_print_button').style.display = 'none';
+}
+function printLog() 
+{
+	document.getElementById('TB_ajaxWindowTitle').innerHTML  = "<h3>Email Content</h3>";
+	document.getElementById('TB_closeWindowButton').style.display = "none";
+	document.querySelectorAll('.check_mail_non-printable').forEach(function(el) {
+		el.style.display = 'none';
+	});
+
+	var divToPrint=document.getElementById('TB_window');
+
+	my_table = document.getElementById('my_table')
+
+	my_table.querySelectorAll('tr').forEach(function(tr) {
+		tr.style.background  = '#eee';
+	});
+	
+
+	my_table.style.border = "1px solid #000"
+	my_table.style.borderWidth = "1px";
+	my_table.style.borderColor = "#000";
+	my_table.style.borderStyle = "solid";
+
+	tabs = document.getElementById('tabs')
+
+	tabs.style.border = "1px solid #000"
+	tabs.style.marginTop = "30px";
+	tabs.style.height = "200px";
+	tabs.style.padding = "20px 10px 20px 20px";;
+	tabs.style.borderWidth = "1px";
+	tabs.style.borderColor = "#000";
+	tabs.style.borderStyle = "solid";
+
+
+	var newWin=window.open('','Print-Window');
+
+	newWin.document.open();
+
+	newWin.document.write('<html><body onload="window.print()">'+divToPrint.innerHTML+'</body></html>');
+
+	newWin.document.close();
+
+	setTimeout(function(){newWin.close();},10);
+	jQuery('.tb-close-icon').click();
+}
--- include/Core/Request/Check_Email_Log_List_Action.php
+++ include/Core/Request/Check_Email_Log_List_Action.php
@@ -18,7 +18,6 @@ class Check_Email_Log_List_Action implements Loadie {
 	}
 
 	public function view_log_message() {
-
 		if ( ! current_user_can( 'manage_check_email' ) ) {
 			wp_die();
 		}
@@ -45,21 +44,29 @@ class Check_Email_Log_List_Action implements Loadie {
 			}
 
 			?>
-			<table style="width: 100%;">
+			<table style="width: 100%;" id="my_table">
 				<tr style="background: #eee;">
-					<td style="padding: 5px;"><?php esc_html_e( 'Sent at', 'check-email' ); ?>:</td>
+					<td style="padding: 5px;"><b><?php esc_html_e( 'Sent at', 'check-email' ); ?></b>:</td>
 					<td style="padding: 5px;"><?php echo esc_html( $log_item['sent_date'] ); ?></td>
 				</tr>
 				<tr style="background: #eee;">
-					<td style="padding: 5px;"><?php esc_html_e( 'To', 'check-email' ); ?>:</td>
+					<td style="padding: 5px;"><b><?php esc_html_e( 'To', 'check-email' ); ?></b>:</td>
 					<td style="padding: 5px;"><?php echo esc_html( $log_item['to_email'] ); ?></td>
 				</tr>
 				<tr style="background: #eee;">
-					<td style="padding: 5px;"><?php esc_html_e( 'Subject', 'check-email' ); ?>:</td>
+					<td style="padding: 5px;"><b><?php esc_html_e( 'Subject', 'check-email' ); ?></b>:</td>
 					<td style="padding: 5px;"><?php echo esc_html( $log_item['subject'] ); ?></td>
 				</tr>
                 <tr style="background: #eee;">
-					<td style="padding: 5px;"><?php esc_html_e( 'Headers', 'check-email' ); ?>:</td>
+					<td style="padding: 5px;"><b><?php esc_html_e( 'From', 'check-email' ); ?></b>:</td>
+					<td style="padding: 5px;"><?php echo esc_html( $headers['from'] ); ?></td>
+				</tr>
+				<tr style="background: #eee;">
+					<td style="padding: 5px;"><b><?php esc_html_e( 'Reply To', 'check-email' ); ?></b>:</td>
+					<td style="padding: 5px;"><?php echo esc_html( $headers['reply_to'] ); ?></td>
+				</tr>
+				<tr style="background: #eee;">
+					<td style="padding: 5px;"><b><?php esc_html_e( 'Headers', 'check-email' ); ?></b>:</td>
 					<td style="padding: 5px;"><?php echo esc_html( $log_item['headers'] ); ?></td>
 				</tr>
 
@@ -68,10 +75,10 @@ class Check_Email_Log_List_Action implements Loadie {
 			</table>
 
 			<div id="tabs">
-				<ul data-active-tab="<?php echo absint( $active_tab ); ?>">
-					<li><a href="#tabs-text"><?php esc_html_e( 'Raw Email Content', 'check-email' ); ?></a></li>
-					<li><a href="#tabs-preview"><?php esc_html_e( 'Preview Content as HTML', 'check-email' ); ?></a></li>
-					<li><a href="#tabs-trigger-data"><?php esc_html_e( 'Triggered Form', 'check-email' ); ?></a></li>
+				<ul data-active-tab="<?php echo absint( $active_tab ); ?>" class="check_mail_non-printable">
+					<li><a href="#tabs-text" onclick='hidePrint();'><?php esc_html_e( 'Raw Email Content', 'check-email' ); ?></a></li>
+					<li><a href="#tabs-preview" onclick='showPrint();'><?php esc_html_e( 'Preview Content as HTML', 'check-email' ); ?></a></li>
+					<li><a href="#tabs-trigger-data" onclick='hidePrint();'><?php esc_html_e( 'Triggered Form', 'check-email' ); ?></a></li>
 				</ul>
 
 				<div id="tabs-text">
@@ -96,10 +103,10 @@ class Check_Email_Log_List_Action implements Loadie {
 				</div>
 			</div>
 
-			<div id="view-message-footer">
+			<div id="view-message-footer" class="check_mail_non-printable">
 				<a href="#" class="button action" id="thickbox-footer-close"><?php esc_html_e( 'Close', 'check-email' ); ?></a>
+				<bitton type="button" class="button button-primary" id="check_mail_print_button" style="margin-top: 10px; display:none;" onclick='printLog();'><?php esc_html_e( 'Print', 'check-email' ); ?></a>
 			</div>
-
 			<?php
 		}
 
